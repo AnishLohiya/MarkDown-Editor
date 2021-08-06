b92bcd99d62365fae2393382d917c0c230014011
Anish Lohiya <anishlohiya196@gmail.com>
2021-08-06 12:05:23 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MarkDownReact from "./MarkDownEditor";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <MarkDownReact />;
 }
 
 export default App;
---
+++ src/MarkDownEditor.js
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import "./styles.css";
+
+const Markdownreact = () => {
+  const [markDown, setMarkDown] = useState("# Markdown Editor...\n## Edit to see magic happen!");
+  return (
+    <>
+      <div className="center-div">
+        <textarea
+          className="left-side"
+          value={markDown}
+          onChange={(e) => setMarkDown(e.target.value)}
+        ></textarea>
+
+        <div className="right-side">
+          <ReactMarkdown>{markDown}</ReactMarkdown>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Markdownreact;
